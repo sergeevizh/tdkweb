c5cf4bc82417d5e9a561cab6b557ab6738883dda
Jan Neuburger <jan.neuburger.jn@gmail.com>
2021-10-18 11:03:24 +0200

    yeah ok fine i made some changes

--- tdkweb-frontend/src/app/components/contact-form/contact-form.component.ts
+++ tdkweb-frontend/src/app/components/contact-form/contact-form.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
+  name: string = ""
+  subj: string = ""
+  msg: string = ""
+  url: string = ""
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  makeUrlString(): string{
+    this.url = 'mailto:office@theater-des-kindes.at?subject='+this.stringToUrl(this.subj!);
+    this.url += '&body=Name:%20' + this.stringToUrl(this.name!);
+    this.url += '%0D%0A' + this.stringToUrl(this.msg!);
+    return this.url;
+  }
+
+  stringToUrl(str: string): string{
+    return str.replace(" ", "%20").replace("\n", "%0D%0A")
   }
 
 }
